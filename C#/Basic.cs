24a1f7c4a08033cb08719d637d4e6b6182e924bc
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-07 13:20:04 +0700

    Create Basic.cs

---
+++ C#/Basic.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GettingInput
+{
+  class Basic
+  {
+    static void Main()
+    {
+      Console.WriteLine("What is your birth year?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You reached 22 years old in 2023!");
+  }
+}
