0d0037547a9550548cf7c0563689bcc7746b142b
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-10 12:02:08 +0700

    Update basic.cpp

--- C++/basic.cpp
+++ C++/basic.cpp
@@ -5,5 +5,13 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!";
+  double tempf;
+  double tempc;
+  
+  std::cout << "Enter the temperature in Fahrenheit:";
+  std::cin >> tempf;
+  
+  tempc = (tempf - 32) / 1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
 }
