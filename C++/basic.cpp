dbd6ad202c922354fa7d63b1f53c273bd73e9891
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-10 10:07:19 +0700

    Update and rename Basic.cpp to basic.cpp

---
+++ C++/basic.cpp
@@ -0,0 +1,9 @@
+/* 
+  Before excuting code, we need to compile the file by typing in terminal: g++ <file_name>.cpp -o <excuted_file_name>, then ./<excuted_file_name>
+  E.g. g++ basic.cpp -o basic, then ./basic
+*/
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+}
