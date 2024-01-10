297948eaf4c24f0734d1204f071fb1b14adc84f9
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-10 18:10:14 +0700

    Update basic.cpp

--- C++/basic.cpp
+++ C++/basic.cpp
@@ -7,7 +7,7 @@
 #include <ctime>
 
 int main() {
-  // First example
+  // Get user inputs
   double tempf;
   double tempc;
   
@@ -16,7 +16,7 @@ int main() {
   tempc = (tempf - 32) / 1.8;
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
 
-  // Second example
+  // Condition: if - else if - else
   srand(time(NULL)); // srand is to seed the random number generator, time(NULL) returns the current time in seconds since the epoch (January 1, 1970, 00:00:00 UTC).
   int coin = rand() % 2; // rand() returns a random number in the range 0 to 32767
   if (coin == 0) {
@@ -26,7 +26,7 @@ int main() {
     std::cout << "Tails\n";
   }
 
-  // Third example
+  // Condition: switch
   int number;
   std::cout << "Enter a number to search for Pokemon: ";
   std::cin >> number;
@@ -44,5 +44,20 @@ int main() {
     default:
       std::cout << "Unknown.\n";
       break;
+  }
+
+  // Loop: while
+  int pin = 0;
+  int tries = 0;
+  
+  std::cout << "BANK OF CPP\n";
+
+  while (pin != 1234 && tries < 3) {
+    std::cout << "Enter your PIN: ";
+    std:cin >> pin;
+    tries++;
+  }
+  if (pin == 1234) {
+    std::cout << "PIN accepted!\nYou now have access.\n";
   }
 }
