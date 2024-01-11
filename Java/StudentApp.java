27e82c499990ea65b4c143f2f18d9832d93e04fb
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-11 12:33:54 +0700

    Update StudentApp.java

--- Java/StudentApp.java
+++ Java/StudentApp.java
@@ -1,3 +1,9 @@
+/* 
+Manage student information:
+- Add new students, ensuring that there are no duplicate student IDs
+- Print a complete list of all enrolled students
+- Retrieve detailed information about any student based on their unique student ID
+*/
 import java.util.Scanner;
 
 class StudentApp {
