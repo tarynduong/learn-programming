212867ff967577eb1e8e2e6e86dc2cc2b8e98b82
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-11 13:57:32 +0700

    Update functions.h

--- C++/functions.h
+++ C++/functions.h
@@ -1,13 +1,19 @@
 // Functions declaration
 void sayHello();
+
 std::string feed_the_cat();
+
 int tenth_power(int num);
+
 std::vector<int> first_three_multiples(int num);
+
 bool is_palindrome(std::string text);
-std::string goodnight1(std::string thing1);
+
 int fancy_number(int num1, int num2);
 int fancy_number(int num1, int num2, int num3);
 int fancy_number(double num1, double num2);
+
+std::string goodnight1(std::string thing1);
 /*
 inline
 std::string goodnight1(std::string thing1) {
