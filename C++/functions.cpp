9e911f529ccd8b6ff73f2568aa3ae0c3eeb44ae1
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-11 13:54:58 +0700

    Update functions.cpp

--- C++/functions.cpp
+++ C++/functions.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <cmath>
 
+// We can create many functions with the same name for different operations - function overloading
+int fancy_number(int num1, int num2) {
+  return num1 - num2 + (num1 * num2);
+}
+
+int fancy_number(int num1, int num2, int num3) {
+  return num1 - num2 - num3 + (num1 * num2 * num3);
+}
+
+int fancy_number(double num1, double num2) {
+  return num1 - num2 + (num1 * num2);
+}
+
 void sayHello() {
   std::cout << "Hi there!\n";
 }
