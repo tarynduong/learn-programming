f6389ded391edefd290bfdd147265bd7cbda9113
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-11 15:51:51 +0700

    Update functions.h

--- C++/functions.h
+++ C++/functions.h
@@ -1,8 +1,10 @@
 // OOP
 class Song {
   std::string title;
+  std::string artist;
 public:
-  void add_title(std::string new_title);
+  Song(std::string new_title, std::string new_artist);
+  std::string get_title();
   std::string get_title();
 };
 
