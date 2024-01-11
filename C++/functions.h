c4f950df8100aedf9cee8bb6c83511c0bf27783e
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-11 13:55:59 +0700

    Update functions.h

--- C++/functions.h
+++ C++/functions.h
@@ -4,6 +4,9 @@ int tenth_power(int num);
 std::vector<int> first_three_multiples(int num);
 bool is_palindrome(std::string text);
 std::string goodnight1(std::string thing1);
+int fancy_number(int num1, int num2);
+int fancy_number(int num1, int num2, int num3);
+int fancy_number(double num1, double num2);
 /*
 inline
 std::string goodnight1(std::string thing1) {
