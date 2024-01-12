9be1ba9d2a5b200cf2d23fd47fc2d236a0bd026e
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-12 15:15:47 +0700

    Update Basic.cs

--- C#/Basic.cs
+++ C#/Basic.cs
@@ -9,5 +9,8 @@ namespace GettingInput
       Console.WriteLine("What is your birth year?");
       string input = Console.ReadLine();
       Console.WriteLine($"You reached 22 years old in 2023!");
+      // Because Console.ReadLine() will always return a string, use built-in method to convert a string to an int
+      Console.WriteLine("What is your favorite number?");
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
   }
 }
