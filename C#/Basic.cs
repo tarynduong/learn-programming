f4193c60b589956d254e86df01825fffdd3f79f5
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2024-01-13 11:58:03 +0700

    Update Basic.cs

--- C#/Basic.cs
+++ C#/Basic.cs
@@ -9,8 +9,12 @@ namespace GettingInput
       Console.WriteLine("What is your birth year?");
       string input = Console.ReadLine();
       Console.WriteLine($"You reached 22 years old in 2023!");
+      
       // Because Console.ReadLine() will always return a string, use built-in method to convert a string to an int
       Console.WriteLine("What is your favorite number?");
       int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+      // Always put the character m after the number to tell C# that it's a decimal. This data type is used mostly for financial metrics
+      decimal revenue = 8347209.7642m;
   }
 }
